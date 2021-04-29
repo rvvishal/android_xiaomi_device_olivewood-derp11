dc1c3138331b413ac3ec849850db39e9a830c8d0
Derpfest_dev <80821792+shield44@users.noreply.github.com>
2021-04-29 18:32:40 +0530

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/derp_olivewood.mk
+
+COMMON_LUNCH_CHOICES := \
+    derp_olivewood-user \
+    derp_olivewood-userdebug \
+    derp_olivewood-eng
---
+++ derp_olivewood.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2019-2020 The LineageOS Project
+# Copyright (C) 2021 DerpFest
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit from olivewood device
+$(call inherit-product, device/xiaomi/olivewood/device.mk)
+
+# Inherit some common DerpFest stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+
+DERP_BUILDTYPE := Official
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := derp_olivewood
+PRODUCT_DEVICE := olivewood
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 8A Dual
+PRODUCT_MANUFACTURER := Xiaomi
+
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="olivewood-user 10 QKQ1.191014.001 V11.0.5.0.QCMMIXM release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
