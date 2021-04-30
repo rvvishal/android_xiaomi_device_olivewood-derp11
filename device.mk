27ba2e733fcb38bec8364f3eee0c67d2cf39930c
Vishal Rajbhar <71400384+rvvishal@users.noreply.github.com>
2021-04-30 18:37:15 +0530

    olive: adding camera hals and flags

--- device.mk
+++ device.mk
@@ -128,11 +128,17 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    camera.msm8937
+
+PRODUCT_PACKAGES += \
+    camera.device@1.0-impl \
+    camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    libgui_vendor \
-    libstdc++.vendor \
-    Snap
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor
+
+    
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
